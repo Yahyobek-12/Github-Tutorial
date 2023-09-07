7cd0e05d2681c97144d25dddb7cd58fae5a01368
Yahyobek <sophiyacai@gmail.com>
2023-09-07 18:42:53 +0300

    Hello New Files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Github-Tutorial
+const LikeColor = prompt("What's Your Favourite color");
+
+const Colors = [];
+Colors = ["What's Your Favourite Color Num #1"];
+Colors = ["What's Your Favourite Color Num #2"];
+Colors = ["What's Your Favourite Color Num #3"];
+
+console.log(Colors);
